c1edc53879d0aefe1445f4ef6f28b0829081a0a5
Momzit <mhlvik001@myuct.ac.za>
2018-08-20 08:46:40 +0200

    Saving to file is a problem

--- Selection.py
+++ Selection.py
@@ -5,33 +5,39 @@ import numpy as np
 from os import rename
 import datetime
 # from astropy.io import ascii
+os.chdir("/home/momentum/Downloads/MomentP/Project/data selection/2001")
 
-#=====================USING PANDAS=====================
-data=pd.read_csv('scanchamp20010101.dat',
-         delim_whitespace=True,
-         na_values='99999.0',
-         parse_dates={'Timestamp':[0,1,2,3]},
-         keep_date_col=True,
-         # date_parser=lambda x: pd.datetime.strptime(x,'%Y %m %d %H %M'),
-         # infer_datetime_format=True,
-         # index_col='Timestamp',
-         )
+# =====================USING PANDAS=====================
+for folder, subfolders,files in os.walk('.'):
+    for file in files:
+        if file.startswith('scan'):
+            ii = int(file[-6:-4])
+            jj = int(file[-8:-6])
+            data=pd.read_csv(file,
+                            delim_whitespace=True,
+                            na_values='99999.0',
+                            parse_dates={'Timestamp':[0,1,2,3]},
+                            keep_date_col=True,
+                            # date_parser=lambda x: pd.datetime.strptime(x,'%Y %m %d %H %M'),
+                            # infer_datetime_format=True,
+                            # index_col='Timestamp',
+                            encoding='cp1252',
+                            )
 
-# format='%Y %m %d %H %M',
-data['Timestamp'] = pd.to_datetime(data['Timestamp'], format='%Y %m %d %H')
-data.index = data['Timestamp']
-del data['Timestamp']
+            # format='%Y %m %d %H %M',
+            data['Timestamp'] = pd.to_datetime(data['Timestamp'], format='%Y %m %d %H')
+            data.index = data['Timestamp']
+            del data['Timestamp']
 
-select = data.loc['2001-01-01 19:00:00']
-# select = data.loc['2001 01 01 19:00:00' : '2001 01 01 23:00:00']
+            start = '2001-%02d-%02d 19:00:00' %(jj, ii)
+            end =  '2001-%02d-%02d 23:00:00' %(jj, ii)
+            # print(end)
 
-select.to_csv('testing.dat', sep=' ', na_rep='NaN', float_format='%.3f')
-#=====================USING NUMPY=====================
-#=====================USING BUILT-IN=====================
-# with open("scanchamp20010101.dat", "r") as f:
-#     for line in f:
-#         data = line.split()
-#         with open("testing.dat", "w+") as n_f:
-#             n_f.write(str(data))
-#             n_f.write("\n")
-#=====================ASTROPY METHOD=====================
+            select = data.loc[start:end]
+            # print(select)
+            # select = data.loc['2001 01 01 19:00:00' : '2001 01 01 23:00:00']
+
+            s = 'testing_%02d%02d'%(jj, ii)
+            s1 = s + '.dat'
+            # print(s1)
+            select.to_csv(s1, sep=' ', na_rep='NaN', float_format='%.3f', index=False)
